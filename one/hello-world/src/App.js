39f8426f5807e1acc7e68544c5d0985f4cff02a8
rahuldivyansh <90146156+rahuldivyansh@users.noreply.github.com>
2023-05-30 16:02:49 +0530

    react files

---
+++ one/hello-world/src/App.js
@@ -0,0 +1,14 @@
+import logo from './logo.svg';
+import './App.css';
+import Greet from './Components/Greet';
+
+function App() {
+  return (
+    <div className="App">{
+      <Greet name ='Divyansh'/>
+      }
+    </div>
+  );
+}
+
+export default App;
